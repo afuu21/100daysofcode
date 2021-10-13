a99f1b8736a49d389c9fca4ea232af59c070d57c
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-10-13 17:59:24 +0530

    Create day_39_pangram-array.cpp

---
+++ day_39_pangram-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> n(26,0);
+        
+        for(char c:sentence)n[c-'a']++;
+        
+        for(int i=0;i<26;i++)
+            if(n[i]==0)return false;
+        
+        return true;
+    }
+};
