560ea8d633e68eb0634bdb6285eb5cf98e967fe7
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2021-12-11 20:04:50 +0530

    Create day_100_ipAddress.cpp

---
+++ day_100_ipAddress.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string output = "";
+        for(int i=0; i<address.length(); i++) {
+            if(address[i] == '.') {
+                output += '[.]'
+            } else {
+                output += address[i];
+            }
+        }
+            return output;
+    }
+};
